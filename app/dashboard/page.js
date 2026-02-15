1821306d976c7e995c82b2a87333d11b83eaccfa
paridhi613 <kalyaneparidhi94@gmail.com>
2026-02-15 17:38:34 +0530

    cleaned the dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,14 +1,41 @@
-
-
 "use client";
 
-import { auth } from "@/src/firebase";
-import { signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { auth, db } from "@/src/firebase";
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/navigation";
+
 
 export default function DashboardPage() {
 
   const router = useRouter();
+
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+
+      if (!user) {
+        router.push("/login");
+        return;
+      }
+
+       // get user data from Firestore
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setUserData(docSnap.data());
+      }
+
+    });
+
+    return () => unsubscribe();
+
+  }, [router]);
+
 
   const logout = async () => {
 
@@ -23,6 +50,18 @@ export default function DashboardPage() {
       <div className="card">
 
         <div className="title">Welcome to CoCreate 🚀</div>
+
+        {userData && (
+          <>
+            <div className="message">
+              Name: {userData.name}
+            </div>
+
+            <div className="message">
+              Email: {userData.email}
+            </div>
+          </>
+        )}
 
         <button className="button" onClick={logout}>
           Logout
