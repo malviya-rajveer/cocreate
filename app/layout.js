26aff6ce84685e720209b1bf01dfb70db9c6f0f8
paridhi613 <kalyaneparidhi94@gmail.com>
2026-02-21 14:50:28 +0530

    made some chnages

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,14 @@
 import "./globals.css";
 
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>
-        {children}
+        
+{children}
+       
+        
       </body>
     </html>
   );
